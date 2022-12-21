546057c335815cdfaa6f2a1f0b9ced6442d71a65
NHN <nhn@al01590446.local>
2022-12-21 13:56:40 +0900

    yds

--- src/main/java/com/example/maventestt/MaventestTApplication.java
+++ src/main/java/com/example/maventestt/MaventestTApplication.java
@@ -12,7 +12,7 @@ public class MaventestTApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!";
+		return "Hello World!!@!@!";
 	}
 
 	public static void main(String[] args) {
