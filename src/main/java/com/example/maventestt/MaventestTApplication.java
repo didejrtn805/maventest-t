61f4390e3f450a66ee5453a0bea5aab61653f5e5
NHN <nhn@AL01590446.local>
2022-12-21 13:56:40 +0900

    Initial commit

---
+++ src/main/java/com/example/maventestt/MaventestTApplication.java
@@ -0,0 +1,22 @@
+package com.example.maventestt;
+
+import jakarta.websocket.server.ServerEndpoint;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class MaventestTApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaventestTApplication.class, args);
+	}
+
+}
